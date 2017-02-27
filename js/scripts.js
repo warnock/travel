6272ce052fbe4821e7c217d3ef82c45a9aadcbf2
Grace Stuart and Megan Warnock <grace@>
2017-02-27 10:01:06 -0800

    add submit.click

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,20 @@ function Location(locationName, landmark, dateOfVisit, notes) {
   this.dateOfVisit = dateOfVisit;
   this.notes = notes;
 }
+
+$(document).ready(function() {
+  $("form#newLocation").submit(function(event) {
+    event.preventDefault();
+    var location = $("input#newLocationName").val();
+    var landmark = $("input#landmark").val();
+    var date = $("input#dateOfVisit").val();
+    var notes = $("input#notes").val();
+
+
+
+    $("input#newLocationName").val();
+    $("input#landmark").val();
+    $("input#dateOfVisit").val();
+    $("input#notes").val();
+  });
+});
