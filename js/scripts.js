d3ffe18f4de618bfae10bd4a50764f76b25aa84e
Grace Stuart and Megan Warnock <megan@>
2017-02-27 11:51:20 -0800

    Update html, styles, and interface logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function Location(locationName, landmark, dateOfVisit, notes) {
-  this.locationName = location;
+  this.locationName = locationName;
   this.landmark = landmark;
   this.dateOfVisit = dateOfVisit;
   this.notes = notes;
@@ -8,14 +8,26 @@ function Location(locationName, landmark, dateOfVisit, notes) {
 $(document).ready(function() {
   $("form#newLocation").submit(function(event) {
     event.preventDefault();
-    var location = $("input#newLocationName").val();
-    var landmark = $("input#landmark").val();
-    var date = $("input#dateOfVisit").val();
-    var notes = $("input#notes").val();
-    $("ul#location").append("<li>" + location + "</li>");
-    $("input#newLocationName").val("");
+    var inputtedLocationName = $("input#locationName").val();
+    var inputtedLandmark = $("input#landmark").val();
+    var inputtedDate = $("input#dateOfVisit").val();
+    var inputtedNotes = $("input#notes").val();
+    var newLocation = new Location(inputtedLocationName, inputtedLandmark, inputtedDate, inputtedNotes);
+    $("ul#location").append("<li><span class='location'>" + inputtedLocationName + "</li>");
+    $("input#locationName").val("");
     $("input#landmark").val("");
     $("input#dateOfVisit").val("");
     $("input#notes").val("");
+
+    $(".location").last().click(function() {
+      debugger;
+      $("#locationDetails").show();
+      $("#locationDetails h2").text(newLocation.locationName);
+console.log(newLocation.locationName);
+      $(".locationName").text(newLocation.locationName);
+      $(".landmark").text(newLocation.landmark);
+      $(".date").text(newLocation.dateOfVisit);
+      $(".notes").text(newLocation.notes);
+    });
   });
 });
