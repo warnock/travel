efe471ed8f2dda27d0a6ec700c46cc9cd1b1716e
Grace Stuart and Megan Warnock <megan@>
2017-02-27 09:54:33 -0800

    add basic files.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function Location(locationName, landmark, dateOfVisit, notes) {
+  this.locationName = location;
+  this.landmark = landmark;
+  this.dateOfVisit = dateOfVisit;
+  this.notes = notes;
+}
